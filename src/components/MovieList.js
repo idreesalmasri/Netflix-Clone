d7f0a92cf1c7a5e9dc5b7b5fc9cb500ce99884f4
idreesalmasri <idrees.massri95@gmail.com>
2022-02-16 15:17:18 +0200

    finished

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,6 +3,7 @@ export default function MovieList(props){
     console.log(props.movies);
     return (
         <>
+        <h1>trending movies List:</h1>
          {
                props.movies.map(ele => {
                 return(
